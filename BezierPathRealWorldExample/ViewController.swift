dbf26467f20d2a667c0d8504c59a9f72498df6e7
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-11-11 01:21:48 +0300

    Demo completed.

--- BezierPathRealWorldExample/ViewController.swift
+++ BezierPathRealWorldExample/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import GGShadowedView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var profileView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.width/2
+        profileImageView.layer.masksToBounds = true
+        infoView.layer.cornerRadius = 24
+        infoView.layer.masksToBounds = true
+     
+        profileView.dropShadow()
     }
 
 
 }
 
+extension UIView {
+    
+    func dropShadow() {
+        layer.masksToBounds = false
+        layer.shadowOffset = CGSize(width: 0, height: 0);
+        layer.shadowOpacity = 0.70
+        layer.shadowRadius = 12
+        layer.shadowColor = UIColor.lightGray.cgColor
+    }
+}
